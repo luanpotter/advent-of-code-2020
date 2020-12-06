38e7e3e49dec27ac0a5a268ea867ac0ef1d1b172
Luan Nico <luanpotter27@gmail.com>
2020-12-05 19:20:39 -0500

    Day 1

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,1 +1,21 @@
-main = putStrLn "Hello, World"
+import System.Environment
+import Data.List
+import Data.Maybe
+
+main = do
+  args <- getArgs
+  content <- readFile (args !! 0)
+  print (process content)
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd as bs = [(a, b) | a <- as, b <- bs]
+
+parseList :: [Char] -> [Int]
+parseList input = (map (\x -> read x :: Int) (lines input))
+
+process :: String -> Int
+process input = let
+  numbers = parseList input
+  pairs = cartProd numbers numbers
+  pair = fromJust (find (\(x, y) -> x + y == 2020) pairs)
+  in (fst pair) * (snd pair)
